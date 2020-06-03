23d200fdbc32813459a6e916c5fa30f4df04d167
abhirup-deb <debabhirup1999@gmail.com>
2020-06-03 22:36:28 +0530

    Index.js modified and components added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import ReactDom from 'react-dom';
 
-const App = function(){
+const App = () => {
   return <div>Hi!</div>;
 }
 
-React.render(App);
+ReactDom.render(<App />,document.querySelector('.container'));
