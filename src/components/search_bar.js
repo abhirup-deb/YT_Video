2095321f8ef2e9910dc03dbc54fda9c09bafe1b1
abhirup-deb <debabhirup1999@gmail.com>
2020-06-03 23:32:20 +0530

    Searchbar implemented

---
+++ src/components/search_bar.js
@@ -0,0 +1,8 @@
+import React,{Component} from 'react';
+
+class Searchbar extends Component {
+  render(){
+    return <input />;
+  }
+}
+export default Searchbar;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import Searchbar from './components/search_bar.js';
+
+const API_KEY = 'AIzaSyAlzc2eW-hK4v_9ouQ6Xdmq_mSUJaEVtNE';
 
 const App = () => {
-  return <div>Hi!</div>;
+  return <div> <Searchbar /> </div>;
 }
 
 ReactDom.render(<App />,document.querySelector('.container'));
