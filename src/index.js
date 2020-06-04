a35cc902a171a38b4be14e8aa10e14f50fa63264
abhirup-deb <debabhirup1999@gmail.com>
2020-06-04 22:35:29 +0530

    Youtube apis added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React,{Component} from 'react';
 import ReactDom from 'react-dom';
 import Searchbar from './components/search_bar.js';
+import YTSearch from 'youtube-api-search_bar';
 
 const API_KEY = 'AIzaSyAlzc2eW-hK4v_9ouQ6Xdmq_mSUJaEVtNE';
 
-const App = () => {
-  return <div> <Searchbar /> </div>;
+class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {videos:[]};
+    YTSearch({key: API_KEY,term: 'surfboards'},(videos) => {
+      this.setState({videos});
+    });
+  }
+  render(){
+    return(
+      <div>
+      <Searchbar />
+      <div>
+    );
+  }
 }
-
 ReactDom.render(<App />,document.querySelector('.container'));
