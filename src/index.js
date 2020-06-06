b1cb11f7967e887ae53cd28e02aa0804b92defbc
abhirup-deb <debabhirup1999@gmail.com>
2020-06-06 23:01:37 +0530

    Video Details Added and made Responsive

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React,{Component} from 'react';
 import ReactDom from 'react-dom';
 import Searchbar from './components/search_bar.js';
 import Videolist from './components/video_list.js';
+import Videodetail from './components/video_detail.js';
 import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyAlzc2eW-hK4v_9ouQ6Xdmq_mSUJaEVtNE';
 
-class App extends Component{
+class App extends Component {
   constructor(props) {
     super(props);
     this.state = {videos:[]};
@@ -18,6 +19,7 @@ class App extends Component{
     return (
       <div>
       <Searchbar />
+      <Videodetail video = {this.state.videos[0]}/>
       <Videolist videos = {this.state.videos}/>
       </div>
     );
